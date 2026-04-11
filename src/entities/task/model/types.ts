e37b4e387b0e2c261876f8b52136734cdf65d3e4
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-04-11 17:48:29 +0200

    chore: create types from documentation

---
+++ src/entities/task/model/types.ts
@@ -0,0 +1,15 @@
+export type TaskStatus = 'todo' | 'inProgress' | 'done';
+
+export type TaskPriority = 'low' | 'medium' | 'high';
+
+export interface Task {
+    id: string;
+    title: string;
+    description?: string;
+    status: TaskStatus;
+    priority: TaskPriority;
+    deadline: string;
+    tags: string[];
+    createdAt: string;
+    updatedAt: string;
+}
