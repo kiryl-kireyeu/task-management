824638d0789ec1e36abb2b796e53dcb8a5da45cc
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-04-11 17:25:55 +0200

    chore: created base route provider

---
+++ src/app/router/router.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import RootLayout from './layouts/root-layout';
+import { CreateTaskPage } from '../../pages/create-task';
+import { EditTaskPage } from '../../pages/edit-task';
+import { TaskDetailsPage } from '../../pages/task-details';
+import { TasksPage } from '../../pages/tasks';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+            {
+                index: true,
+                element: <TasksPage />,
+            },
+            {
+                path: 'task/:id',
+                element: <TaskDetailsPage />,
+            },
+            {
+                path: 'create',
+                element: <CreateTaskPage />,
+            },
+            {
+                path: 'edit/:id',
+                element: <EditTaskPage />,
+            },
+        ],
+    },
+]);
+
+export default router;
