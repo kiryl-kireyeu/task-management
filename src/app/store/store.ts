7e0a5c31ce231eda4a7dbb966e3959ab8cba7074
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-04-12 11:07:04 +0200

    feat: create rtk api provider for request server

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { taskApi } from '../../entities/task/api/api';
+
 // Store is kept minimal and will host RTK Query state later.
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [taskApi.reducerPath]: taskApi.reducer,
+    },
+    middleware: (gDM) => gDM().concat(taskApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
