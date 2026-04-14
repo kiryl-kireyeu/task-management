4483fd9bbda78a0912a1857b11db74af78191e39
Kiryl Kireyeu <kirylkireyeu@gmail.com>
2026-04-14 16:59:25 +0200

    feat: add form to page create task

--- src/pages/create-task/index.tsx
+++ src/pages/create-task/index.tsx
@@ -1,10 +1,15 @@
-import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+
+import TaskForm from '@/features/task-form/ui/task-form';
+import PageShell from '@/shared/ui/page-shell';
 
 const CreateTaskPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <Typography component="h1" gutterBottom variant="h4">
-            Create Task
-        </Typography>
+        <PageShell maxWidth="sm" onBack={() => navigate(-1)} title="Создать задачу">
+            <TaskForm onSubmit={() => {}} submitButtonText="Создать" />
+        </PageShell>
     );
 };
 
